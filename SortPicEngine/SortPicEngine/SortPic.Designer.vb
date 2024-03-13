cd92130b995d519bf81c9b2562ac09aa997c15a1
Nuatury <46191006+Nuatury@users.noreply.github.com>
2024-03-13 19:22:19 +0800

    自动图片分类初版
    自动图片分类初版

---
+++ SortPicEngine/SortPicEngine/SortPic.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SortPic
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(685, 132)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "获取宽高"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(768, 83)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox2.Size = New System.Drawing.Size(94, 28)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "3"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(685, 247)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(177, 43)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "检查错误"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(685, 187)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(177, 43)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "获取名字"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(688, 442)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(177, 43)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "执行排序"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(31, 83)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(613, 405)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.WordWrap = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(685, 414)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(88, 22)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "只预览"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(682, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 18)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "序号长度"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(132, 30)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(559, 28)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 18)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "文件夹路径"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(715, 30)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(138, 37)
+        Me.Button5.TabIndex = 12
+        Me.Button5.Text = "设置路径"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(39, 27)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(105, 22)
+        Me.RadioButton1.TabIndex = 13
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "以宽排序"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(685, 296)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(220, 99)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "排序"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(39, 55)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(105, 22)
+        Me.RadioButton2.TabIndex = 14
+        Me.RadioButton2.Text = "以高排序"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'SortPic
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(917, 518)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "SortPic"
+        Me.Text = "图片像素排序工具"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ SortPicEngine/SortPicEngine/SortPic.vb
@@ -0,0 +1,210 @@
+﻿Imports System.IO
+Imports System.Drawing
+
+
+
+Public Class SortPic
+
+
+    Public Function id_str(ByVal n As Integer, ByVal prelenth As Integer) As String
+        Dim str_n = n.ToString
+        Dim str_prelenth = prelenth.ToString
+        If Len(str_n) > str_prelenth Then
+            Debug.WriteLine("目前序号数字长度大于预期长度")
+            MsgBox("文件数量位数 > 序号长度" + vbCrLf + Len(str_n).ToString + ">" + str_prelenth)
+            Return False
+
+        End If
+        Do While Len(str_n) < str_prelenth
+            str_n = "0" + str_n
+            If Len(str_n) = str_prelenth Then
+                Return str_n
+            End If
+        Loop
+        Return ""
+    End Function
+
+    Public Function find_max(ByVal array1 As Array) As Integer
+        Dim v_max = 0
+        If array1.Length > 0 Then
+            For Each vaule In array1
+                If vaule > v_max Then
+                    v_max = vaule
+                End If
+            Next
+            Debug.WriteLine(v_max)
+            Return v_max
+        End If
+        Debug.WriteLine(0)
+        Return 0
+    End Function
+
+    Public Function find_error()
+
+        If TextBox3.Text = "" Then
+            MsgBox("空路径，撒比")
+            Return 0
+        End If
+
+        Dim dDir As New DirectoryInfo(TextBox3.Text)
+        If Not dDir.Exists Then
+            MsgBox("没找到这个路径，撒比")
+            Return 0
+        End If
+
+        Dim s As New DirectoryInfo(TextBox3.Text)
+        Dim files As FileInfo() = s.GetFiles("*.png")
+        Dim count = CType(TextBox2.Text, Integer)
+        If Len(files.Length.ToString) > count Then
+            MsgBox("文件数量位数 > 序号长度" + vbCrLf + Len(files.Length.ToString).ToString + ">" + count.ToString)
+            Return 0
+        End If
+        MsgBox("OK!")
+        Return 1
+
+    End Function
+
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim s As New DirectoryInfo(TextBox3.Text)
+        Dim files As FileInfo() = s.GetFiles("*.png")
+        'Debug.WriteLine("shabi")
+
+        Dim n = 1
+        Dim prelenth = TextBox2.Text
+        If prelenth = "" Then
+            prelenth = "3"
+        End If
+
+        Dim str1 = ""
+        For Each f As FileInfo In files
+            Dim bmp As New Bitmap(f.FullName)
+
+            Debug.WriteLine("PIC_" + id_str(n, prelenth) + ":" + "Width: " & bmp.Width.ToString() + " > Height: " & bmp.Height.ToString())
+            n += 1
+            str1 = str1 + "PIC_" + id_str(n, prelenth) + ":" + "Width: " & bmp.Width.ToString() + " > Height: " & bmp.Height.ToString() + vbCrLf
+
+        Next
+        TextBox1.Text = str1
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        find_error()
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim s As New DirectoryInfo(TextBox3.Text)
+        Dim files As FileInfo() = s.GetFiles("*.png")
+
+        Dim n = 1
+        Dim prelenth = TextBox2.Text
+        If prelenth = "" Then
+            prelenth = "3"
+        End If
+        Dim f_name_no_extension() As Array
+        '返回文件名不包含扩展名
+
+        Dim str1 = ""
+        For Each f As FileInfo In files
+            Dim f_name = f.Name.Replace(f.Extension, "")
+            'f_name_no_extension(1) = 1
+            Debug.WriteLine(f_name)
+            Dim bmp As New Bitmap(f.FullName)
+            'Debug.WriteLine("Width: " & bmp.Width.ToString() + " > Height: " & bmp.Height.ToString())
+
+            str1 = str1 + f_name + vbCrLf
+        Next
+        TextBox1.Text = str1
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If find_error() = 0 Then
+            Debug.WriteLine("break")
+            Return
+        End If
+
+        Dim s As New DirectoryInfo(TextBox3.Text)
+        Dim files As FileInfo() = s.GetFiles("*.png")
+        Dim l_files = files.Length
+        Debug.WriteLine(l_files.GetType)
+        Dim f_name(files.Length) As String
+        Dim f_width(files.Length) As Integer
+        Dim f_height(files.Length) As Integer
+        Dim f_extension(files.Length) As String
+        For i = 0 To files.Length - 1
+            Using bmp As New Bitmap(files(i).FullName)
+                f_name(i) = files(i).Name '.Replace(files(i).Extension, "")
+                f_width(i) = bmp.Width
+                f_height(i) = bmp.Height
+                f_extension(i) = files(i).Extension
+            End Using
+
+        Next
+        Dim f_sort(files.Length) As Integer
+        Dim f_width2 = f_width
+        Dim f_height2 = f_height
+        Dim width_max As Integer = find_max(f_width2)
+        Dim height_max As Integer = find_max(f_height2)
+        Dim index = 0
+
+        If RadioButton1.Checked Then 'kuan
+            Do While width_max > 0
+                For i = 0 To files.Length - 1
+                    If f_width2(i) = width_max Then
+                        f_width2(i) = 0
+                        f_sort(index) = i
+                        index += 1
+                    End If
+                Next
+                width_max = find_max(f_width2)
+            Loop
+        ElseIf RadioButton2.Checked Then 'gao
+            Do While height_max > 0
+                For i = 0 To files.Length - 1
+                    If f_height2(i) = height_max Then
+                        f_height2(i) = 0
+                        f_sort(index) = i
+                        index += 1
+                    End If
+                Next
+                height_max = find_max(f_height2)
+            Loop
+        End If
+
+
+        Dim str1 = ""
+        Dim str2 = ""
+        For j = 0 To f_sort.Length - 2
+            str1 = str1 + f_sort(j).ToString + " "
+
+            Dim oldname = files(f_sort(j)).FullName
+            Dim newname = id_str(j + 1, CType(TextBox2.Text, Integer)) + "_" + f_name(f_sort(j)) 'files(f_sort(j)).DirectoryName + "\" + 
+            Debug.WriteLine(oldname + " " + newname)
+            str2 = str2 + oldname + " --> " + newname + vbCrLf
+            If CheckBox1.Checked = False Then
+                My.Computer.FileSystem.RenameFile(oldname, newname)
+            End If
+        Next
+
+
+
+        Debug.WriteLine(str1)
+        TextBox1.Text = str2
+
+
+
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        FolderBrowserDialog1.ShowDialog()
+        TextBox3.Text = FolderBrowserDialog1.SelectedPath
+
+    End Sub
+End Class
+
+
